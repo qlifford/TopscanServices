ea47d0dc26140fb99b26815d9f58dbc45794e5da
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-07-09 14:58:06 +0300

    main

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LogoutController extends Controller
+{
+    public function store()
+    {
+        auth()->logout();
+    }
+}
